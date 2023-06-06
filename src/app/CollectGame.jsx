a2b7e6ff3634d8d57daada157cdde2f550ce256d
Haibin Xiang <xianghb1987@gmail.com>
2023-06-06 14:17:57 +0200

    update

--- src/app/CollectGame.jsx
+++ src/app/CollectGame.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import LetterPanelContainer from './letter-panel/letter-panel-container';
-import ScorePanelContainer from './score-panel/score-panel-container';
-import './styles.less';
+import React from "react";
+import LetterPanelContainer from "./letter-panel/letter-panel-container";
+import ScorePanelContainer from "./score-panel/score-panel-container";
+import "./styles.less";
 
 const CollectGame = () => (
   <div className="game-container">
--- src/app/score-panel/score-panel.jsx
+++ src/app/score-panel/score-panel.jsx
@@ -1,4 +1,3 @@
-// @flow
 import React from "react";
 import HeaderTitle from "../component/header-title/header-title";
 import ScoreListContainer from "./score-list/score-list-container";
